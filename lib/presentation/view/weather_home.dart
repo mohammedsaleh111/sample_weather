043d62ba1084aa70db1a529b515ef8f3eb59f820
mohammedsaleh111 <salehhamada664@gmail.com>
2024-10-16 15:30:06 +0300

    initial project structure

---
+++ lib/presentation/view/weather_home.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class WeatherHome extends StatelessWidget {
+  const WeatherHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
